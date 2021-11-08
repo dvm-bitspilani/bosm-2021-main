e933fbd078d4fafd04dc83a676bd2ff5467ed144
Saksham Aggarwal <f20201508@pilani.bits-pilani.ac.in>
2021-11-08 09:02:57 +0530

    done

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,47 @@
-function changeText() {
-  console.log("I am here");
-  if (
-    document.getElementsByClassName("student")[0].value === "School Student"
-  ) {
-    console.log("I'm school");
-    document.getElementsByClassName("studyYear")[0].innerHTML = "Class";
-    document.querySelector(".changeYear option:nth-child(1)").selected = true;
-    document.getElementsByClassName("className")[0].innerHTML =
-      "Select Your Class";
-    document.getElementById("hideGrad").style.display = "initial";
-    document.querySelector(".changeYear option:nth-child(1)").innerHTML =
-      "Class";
-    document.querySelector(".changeYear option:nth-child(2)").disabled = false;
-    document.querySelector(".changeYear option:nth-child(3)").disabled = false;
-    document.querySelector(".changeYear option:nth-child(4)").disabled = false;
-    document.querySelector(".changeYear option:nth-child(5)").disabled = true;
-    document.querySelector(".changeYear option:nth-child(6)").disabled = true;
-    document.querySelector(".changeYear option:nth-child(7)").disabled = true;
-    document.querySelector(".changeYear option:nth-child(8)").disabled = true;
-    document.querySelector(".changeYear option:nth-child(9)").disabled = true;
-  } else if (
-    document.getElementsByClassName("student")[0].value === "Graduated"
-  ) {
-    document.getElementById("hideGrad").style.display = "none";
-  } else {
-    document.getElementsByClassName("studyYear")[0].innerHTML = "Year of Study";
-    document.getElementById("hideGrad").style.display = "initial";
-    document.querySelector(".changeYear option:nth-child(1)").selected = true;
-    document.querySelector(".changeYear option:nth-child(1)").innerHTML =
-      "Year of Study";
-    document.querySelector(".changeYear option:nth-child(2)").disabled = true;
-    document.querySelector(".changeYear option:nth-child(3)").disabled = true;
-    document.querySelector(".changeYear option:nth-child(4)").disabled = true;
-    document.querySelector(".changeYear option:nth-child(5)").disabled = false;
-    document.querySelector(".changeYear option:nth-child(6)").disabled = false;
-    document.querySelector(".changeYear option:nth-child(7)").disabled = false;
-    document.querySelector(".changeYear option:nth-child(8)").disabled = false;
-    document.querySelector(".changeYear option:nth-child(9)").disabled = false;
+// function changeText() {
+//   console.log("I am here");
+//   if (
+//     document.getElementsByClassName("student")[0].value === "School Student"
+//   ) {
+//     console.log("I'm school");
+//     document.getElementsByClassName("studyYear")[0].innerHTML = "Class";
+//     document.querySelector(".changeYear option:nth-child(1)").selected = true;
+//     document.getElementsByClassName("className")[0].innerHTML =
+//       "Select Your Class";
+//     document.getElementById("hideGrad").style.display = "initial";
+//     document.querySelector(".changeYear option:nth-child(1)").innerHTML =
+//       "Class";
+//     document.querySelector(".changeYear option:nth-child(2)").disabled = false;
+//     document.querySelector(".changeYear option:nth-child(3)").disabled = false;
+//     document.querySelector(".changeYear option:nth-child(4)").disabled = false;
+//     document.querySelector(".changeYear option:nth-child(5)").disabled = true;
+//     document.querySelector(".changeYear option:nth-child(6)").disabled = true;
+//     document.querySelector(".changeYear option:nth-child(7)").disabled = true;
+//     document.querySelector(".changeYear option:nth-child(8)").disabled = true;
+//     document.querySelector(".changeYear option:nth-child(9)").disabled = true;
+//   } else if (
+//     document.getElementsByClassName("student")[0].value === "Graduated"
+//   ) {
+//     document.getElementById("hideGrad").style.display = "none";
+//   } else {
+//     document.getElementsByClassName("studyYear")[0].innerHTML = "Year of Study";
+//     document.getElementById("hideGrad").style.display = "initial";
+//     document.querySelector(".changeYear option:nth-child(1)").selected = true;
+//     document.querySelector(".changeYear option:nth-child(1)").innerHTML =
+//       "Year of Study";
+//     document.querySelector(".changeYear option:nth-child(2)").disabled = true;
+//     document.querySelector(".changeYear option:nth-child(3)").disabled = true;
+//     document.querySelector(".changeYear option:nth-child(4)").disabled = true;
+//     document.querySelector(".changeYear option:nth-child(5)").disabled = false;
+//     document.querySelector(".changeYear option:nth-child(6)").disabled = false;
+//     document.querySelector(".changeYear option:nth-child(7)").disabled = false;
+//     document.querySelector(".changeYear option:nth-child(8)").disabled = false;
+//     document.querySelector(".changeYear option:nth-child(9)").disabled = false;
 
-    document.getElementsByClassName("className")[0].innerHTML =
-      "Select Your Year of Study";
-  }
-}
+//     document.getElementsByClassName("className")[0].innerHTML =
+//       "Select Your Year of Study";
+//   }
+// }
 
 var selected_sport = document.getElementsByClassName("selected-sports")[0];
 var sports_opt = document.getElementById("sports_opt");
@@ -52,56 +52,25 @@ var gender_value;
 var collegeid;
 var no_of_sports = 4;
 var is_coach = false;
-// var membersArr = [
-//   {
-//     name: "Mohit Makwana",
-//     email: "f2020xxxx@pilani.bits-pilani.ac.in",
-//     phone: "8810608387",
-//     bits_id: "2020A7PS0048P",
-//     gender: "male",
-//   },
-//   {
-//     name: "Mohit Makwana 2",
-//     email: "f2020xxxx1@pilani.bits-pilani.ac.in",
-//     phone: "8810608387",
-//     bits_id: "2020A7xxx3P",
-//     gender: "male",
-//   },
-//   {
-//     name: "Mohit Makwana 3",
-//     email: "f2020xxx2@pilani.bits-pilani.ac.in",
-//     phone: "8810608387",
-//     bits_id: "2020A7xxx2",
-//     gender: "male",
-//   },
-//   {
-//     name: "Mohit Makwana 4",
-//     email: "f2020xxx3@pilani.bits-pilani.ac.in",
-//     phone: "8810608387",
-//     bits_id: "2020A7xxxxP",
-//     gender: "male",
-//   },
-// ];
-
 let membersArr = [];
 
 
 /// This is for showing the rules of the game 
 
-function getsportsvalue() {
-  const URL = "https://bits-bosm.org/bosm2021/registrations/sports_rules";
-  fetch(URL, {
-    method: "GET", 
-    body: document.querySelector("#sports_opt").value
-  })
-  .then((resp) => resp.json())
-  .then(function (resp) {
-      console.log(resp);
-  })
-  .catch(function (error) {
-      console.log(error);
-  });
-}
+// function getsportsvalue() {
+//   const URL = "https://bits-bosm.org/bosm2021/registrations/sports_rules";
+//   fetch(URL, {
+//     method: "GET", 
+//     body: document.querySelector("#sports_opt").value
+//   })
+//   .then((resp) => resp.json())
+//   .then(function (resp) {
+//       console.log(resp);
+//   })
+//   .catch(function (error) {
+//       console.log(error);
+//   });
+// }
 
 
 //Lite
@@ -117,66 +86,42 @@ function getcollegeid() {
 // FORM Submission
 
 function bosmreg() {
-  if (document.getElementById("no").checked) {
-    console.log(document.getElementsByName("coach")[0].value);
-    alert("You need to be above 18 to apply");
-  } else {
+  
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const phone = document.getElementById("phone").value;
-    const city = document.getElementById("city").value;
-    const state = document.getElementById("state").value;
-    const college = document.getElementById("college").value;
-    const yearofstudy = document.getElementById("yearofstudy").value;
-    const aadharFile = document.getElementById("myfile").files[0];
+    const sport = document.getElementById("sports_opt").value;
+    const gender = document.getElementById("member-gender").value;
+    const bits_id = document.getElementById("bits-id").value;
+    const team_name = "d";
 
-    console.log(aadharFile);
-    const occupation = document.getElementById("occupation").value;
+    const data ={
+      name: name,
+      email: email,
+      phone: phone,
+      bits_id: bits_id,
+      game: sport,
+      gender: gender,
+      team_name : team_name,
+      members: membersArr,
+    }
 
-    var aadharFormData = new FormData();
-    aadharFormData.append("file", aadharFile);
-    aadharFormData.append("name", name);
-    aadharFormData.append("email", email);
-    aadharFormData.append("phone", phone);
-    aadharFormData.append("city", city);
-    aadharFormData.append("state", state);
-    aadharFormData.append("college", college);
-    aadharFormData.append("occupation", occupation);
-    aadharFormData.append("is_eighteen", true);
-    aadharFormData.append("games_ids", sportsarr);
-    aadharFormData.append("year_of_study", yearofstudy);
 
     if (
       name == "" ||
       email == "" ||
-      phone == "" ||
-      city == "" ||
-      state == "" ||
-      sportsarr == [] ||
-      aadharFile == null
+      phone == ""
       // collegeid == null ||
       // yos_value == null ||
       // gender_value == null
-    ) {
+    ){
       alert("Please fill all mentioned feilds");
     } else {
-      data = {
-        name: name,
-        email: email,
-        phone: phone,
-        college: college,
-        year_of_study: yearofstudy,
-        games_ids: sportsarr,
-        city: city,
-        state: state,
-        occupation: occupation,
-        is_eighteen: true,
-        file: aadharFormData,
-      };
 
       fetch("https://bits-bosm.org/bosm2021/registrations/register/", {
+        headers: {'content-type': 'application/json'},
         method: "PUT",
-        body: aadharFormData,
+        body: data,
       })
         .then(function (response) {
           return response.json();
@@ -187,10 +132,9 @@ function bosmreg() {
         .catch(function (error) {
           console.log(error);
         });
-      console.log(aadharFormData);
     }
-  }
 }
+
 
 window.onload = function () {
   document.querySelector(".spinner").style.display = "none";
@@ -251,7 +195,7 @@ function showMember(memberDetails) {
     node.appendChild(textnode); // Append the text to <li>
     document.getElementById("members").appendChild(node);
   }
-  
+
 }
 
 // showMember(membersArr);
@@ -264,23 +208,38 @@ function showNewMemberForm() {
 
 function addTeamMember() {
   document.querySelector(".add-team-member").style.display = "none";
-  const teamname = document.getElementById("team-name").value;
+  const teamMemberName = document.getElementById("team-member-name").value;
   const teamemail = document.getElementById("team-email").value;
   const teambitsid = document.getElementById("team-bitsid").value;
   const teamphone = document.getElementById("team-phone").value;
   const membergender = document.getElementById("team-member-gender").value;
 
-  console.log(teamname)
-
   let memberDetails = {
-    name: teamname,
+    name: teamMemberName,
     email: teamemail,
     phone: teamphone,
     bits_id: teambitsid,
     gender: membergender,
   }
-  membersArr.push(memberDetails);
-  console.log(membersArr)
 
-  showMember(memberDetails);
+  if (
+    teamMemberName == "" ||
+    teamemail == "" ||
+    teambitsid == "" ||
+    teamphone == "" ||
+    membergender == ""
+    // collegeid == null ||
+    // yos_value == null ||
+    // gender_value == null
+  ) {
+    alert("Please fill all details of member.");
+  }else{
+    
+  
+    membersArr.push(memberDetails);
+    console.log(membersArr)
+  
+    showMember(memberDetails);
+  }
+  
 }
