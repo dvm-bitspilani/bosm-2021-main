15dc2f9c0142c85cc22bd448fee12f64030d7e37
sarthakeash <sarthakeash@yahoo.in>
2021-11-21 16:04:50 +0530

    api done

--- ebosm/scripts/fixtures.js
+++ ebosm/scripts/fixtures.js
@@ -1,50 +1,65 @@
-for (let k = 0; k < 4; k++) {
-  let gameName;
-  k == 0
-    ? (gameName = "valo")
-    : k == 1
-      ? (gameName = "csgo")
-      : k == 2
-        ? (gameName = "codm")
-        : (gameName = "fifa");
+async function fetchData() {
+  for (let k = 0; k < 4; k++) {
+    let gameName;
+    k == 0
+      ? (gameName = "valo")
+      : k == 1
+        ? (gameName = "csgo")
+        : k == 2
+          ? (gameName = "codm")
+          : (gameName = "fifa");
 
-  const allDates = await fetch(
-    `https://bits-bosm.org/bosm2021/registrations/bosm_fixtures/${gameName}/`
-  );
-  const data = await allDates.json();
-  console.log(data);
-  for (let i = 0; i < 4; i++) {
-    data[i].map(({ game, teamA, teamB, scoreA,scoreB, time }) => {
-      let gameCard = document.createElement("div");
-      let dateObj = new Date(time);
-      if ((new Date()).getDate() <= dateObj.getDate()) {
-        gameCard.className = "fixture-card";
-        gameCard.innerHTML = `
-  <h6>${game} match</h6>
+    const allDates = await fetch(
+      `https://bits-bosm.org/bosm2021/registrations/bosm_fixtures/${gameName}/`
+    );
+    const data = await allDates.json();
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      data[i].map(({ Game, teamA, teamB, score, Time, Type }) => {
+    
+        let matchType;
+       switch (Type) {
+          case 0: matchType = "Normal Match"
+            break;
+         case 1: matchType = "Quarter Finals"
+           break;
+         case 2: matchType = "Semi Finals"
+           break;
+case 3: matchType="Finals"
+        }
+        let scoreA = score.split('-')[0];
+        let scoreB = score.split('-')[1];
+
+        let gameCard = document.createElement("div");
+        let dateObj = new Date(Time);
+        if ((new Date()).getDate() <= dateObj.getDate()) {
+          gameCard.className = "fixture-card";
+          gameCard.innerHTML = `
+  <h6>${matchType}</h6>
   <div class="match-info">
       <div class="match-teams">
           <p>${teamA}</p>
           <p>${teamB}</p>
       </div>
       <div class="match-date">
-          <p>${dateObj.toString().split(' ')[0]}<span class="match-date-no">${dateObj.getDate()}</span> ${dateObj.toLocaleString("default", {
-          month: "short",
-        })}</p>
+          <p>${dateObj.toString().split(' ')[0]} <span class="match-date-no">${dateObj.getDate()}</span> ${dateObj.toLocaleString("default", {
+            month: "short",
+          })}</p>
           <h4 class="match-time-no">${dateObj.getHours() + ":" + dateObj.getMinutes()
-          }</h4>
+            }</h4>
       </div>
 
 
-  </div>>`;
-      }
+  </div>`;
+        }
 
-      else {
-        gameCard.className = "fixture-card past-events";
-        gameCard.innerHTML = `
+        else {
+          gameCard.className = "fixture-card past-events";
+          gameCard.innerHTML = `
         <div style="display:flex; justify-content:space-between">
-        <h6>${game} match</h6> <span>${dateObj.toString().split(' ')[0]}, ${dateObj.getDate()} + ${dateObj.toLocaleString("default", {
-          month: "short",
-        })} </span>
+        <h6>${matchType}</h6> <span>${dateObj.toString().split(' ')[0]}, ${dateObj.getDate()} + ${dateObj.toLocaleString("default", {
+            month: "short",
+          })} </span>
     </div>
     <div class="match-info">
         <div class="match-teams">
@@ -57,14 +72,17 @@ for (let k = 0; k < 4; k++) {
         </div>
     </div>
 
-    <h6 style="text-align: center;width: 100%;">${(scoreA>scoreB)?teamA:teamB} <span
+    <h6 style="text-align: center;width: 100%;">${(scoreA > scoreB) ? teamA : teamB} <span
             style="font-family:'affirmative'">WON</span></h6>
-`;   
+`;
 
-}
+        }
         document
-          .getElementById(`${gameName + (i + 1)}`)
+          .getElementById(gameName+ '-'+ (i+1))
           .appendChild(gameCard);
-            });
+      });
+    }
   }
 }
+
+fetchData();
